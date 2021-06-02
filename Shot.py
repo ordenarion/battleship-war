486e705891ddcc6534cbe25eb2d8a14c3a3d5eb6
DmitryBoyko147 <70600959+DmitryBoyko147@users.noreply.github.com>
2021-06-02 08:51:33 +0300

    Исправлена функция выстрела

--- Shot.py
+++ Shot.py
@@ -17,22 +17,19 @@ def shot(list,ships,x,y):
         if right_coordinates(x+1,y+1):
             list[x+1][y+1]=-3
         positions=[]
-        t1=False
-        t2=False
-        t3=False
-        t4=False
+        
         if right_coordinates(x,y-1) and list[x][y-1]>-2:
             positions.append([x,y-1])
-            t1=list[x][y-1]==1
+            
         if right_coordinates(x,y+1) and list[x][y+1]>-2:
             positions.append([x,y+1])
-            t2=list[x][y+1]==1
+            
         if right_coordinates(x-1,y) and list[x-1][y]>-2:
             positions.append([x-1,y])
-            t3=list[x-1][y]==1
+            
         if right_coordinates(x+1,y) and list[x+1][y]>-2:
             positions.append([x+1,y])
-            t4=list[x+1][y]==1
+            
             
         sh=0
         while not [x,y,state1] in ships[sh]:
@@ -44,21 +41,10 @@ def shot(list,ships,x,y):
             
         destroyed=sum==-2*len(ships[sh])    
         
-        if len(positions)==0 and destroyed:
-            return "destroyed"
-        elif (t1 or t2 or t3 or t4)==False and destroyed:
-            if list[x][y-1]!=-2:
-                list[x][y-1]=-3
-            if list[x][y+1]!=-2:
-                list[x][y+1]=-3
-            if list[x-1][y]!=-2:
-                list[x-1][y]=-3
-            if list[x+1][y]!=-2:
-                list[x+1][y]=-3
+        if destroyed:
+                for i in range(len(positions)):
+                        list[positions[i][0]][positions[i][1]]=-3
 
-            return "destroyed"
-        elif len(positions)==0 or (t1 or t2 or t3 or t4)==False: 
-            return "alive but no list"
-            
+                return "destroyed"     
         else:
             return positions
